f355bd8fb63295979a4ed0122da137642c94bcf7
mehdi kibous <mehdikib321@gmail.com>
2024-09-25 14:30:24 +0100

    tac

--- day002/script.js
+++ day002/script.js
@@ -1,12 +1,15 @@
-function change_img(id1, id2, id3, src){
-    let i1 = document.getElementById(id1);
-    let i2 = document.getElementById(id2);
-    let i3 = document.getElementById(id3);
-    i1.style.border = "2px solid #44AADD";
-    i1.style.opacity = "1";
-    i2.style.border = "none";
-    i2.style.opacity = "0.5";
-    i3.style.border = "none";
-    i3.style.opacity = "0.5";
-    document.getElementById("big_img").src = src;
+
+let images = document.querySelectorAll(".im");
+for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener("click", function () {
+        for (let j = 0; j < images.length; j++) {
+            if (images[j] === images[i]) {
+                images[j].classList.add("selected");
+                document.getElementById("big_img").src = images[j].src;
+            }
+            else {
+                images[j].classList.remove("selected");
+        }}
+    });
 }
+
