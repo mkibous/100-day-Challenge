db3714d8c8357c699ede368be0a4b686f57eef99
mehdi kibous <mehdikib321@gmail.com>
2024-10-12 18:46:30 +0100

    days

--- day013 task board/script.js
+++ day013 task board/script.js
@@ -1,4 +1,5 @@
 let dificult = document.querySelectorAll(".dificult");
+let todoelement = document.querySelectorAll(".todo");
 dificult.forEach((element) => {
     if (element.textContent === "LOW")
         element.classList.add("bg-green-500");
@@ -7,3 +8,61 @@ dificult.forEach((element) => {
     else
         element.classList.add("bg-red-500");
     });
+let form = document.getElementById("form");
+function displayForm() {
+    form.classList.toggle("hidden");
+    if (!form.classList.contains("hidden")) {
+        todoelement.forEach((element) => {
+            sildeIn(element);
+        });
+    }
+    else {
+        todoelement.forEach((element) => {
+            slidetop(element);
+        });
+    }
+}
+function sildeIn(classname)
+{
+classname.animate([
+	{
+		offset: 0,
+		transform: "translateY(-400px)"
+	},
+	{
+		offset: 1,
+		transform: "translateY(0px)"
+	}
+],{				 
+	duration: 400,
+	easing: 'linear',
+	delay: 0,
+	iterations: 1,
+	direction: 'normal',
+	fill: 'none'
+});
+}
+function slidetop(classname)
+{
+
+classname.animate(
+  [
+    {
+      offset: 0,
+      transform: "translateY(400px)",
+    },
+    {
+      offset: 1,
+      transform: "translateY(0px)",
+    },
+  ],
+  {
+    duration: 400,
+    easing: "linear",
+    delay: 0,
+    iterations: 1,
+    direction: "normal",
+    fill: "none",
+  }
+);
+}
